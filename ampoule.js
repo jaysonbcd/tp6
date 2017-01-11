14b48635df9acccabdbcff8e931374108c16c61f
boucaud.jayson <boucaud.jayson@gmail.com>
2017-01-11 05:02:11 -0500

    tp6

---
+++ ampoule.js
@@ -0,0 +1,9 @@
+function allumer(){
+  var img = document.getElementById("on");
+  if(img.src=="http://www.iut-fbleau.fr/sitebp/web/wim11/js/on.png"){
+    img.src="http://www.iut-fbleau.fr/sitebp/web/wim11/js/off.png";
+  }
+  else{
+  img.src="http://www.iut-fbleau.fr/sitebp/web/wim11/js/on.png";
+  }
+}
